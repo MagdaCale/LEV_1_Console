2cb212416ffa65b76f4f534f826623e5308ef98e
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-07-14 14:22:21 +0200

    LEV_1 Console

---
+++ assets/JS/main.js
@@ -0,0 +1,25 @@
+//let a = 12
+//let b = a * 3
+//console.log(b);
+
+
+console.log(document.URL);
+console.log('Helle World!')
+console.log('Anton')
+
+let firstName = 'Anton'
+console.log('firstName')
+
+let age = 28
+console.log('age')
+
+let job = 'Trainer'
+console.log('job')
+
+let married = 'true'
+console.log('married')
+
+console.log(firstName + ' ist ' + age + ' alt, von Beruf ' + job + ' und ist Verheiratet ' + married)
+
+
+
